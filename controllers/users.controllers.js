5a7b9b257a39789e3edb1b3a4bc64deba447812a
robertopeiro12 <robertopeiro12@gmail.com>
2025-04-24 10:43:17 -0600

    feat: add database connection pool configuration

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -12,7 +12,16 @@ export const getUsers = (req, res) => {
         res.status(200).json({msg: "OK",  users: results });
 });
 };
-export const getUser = (req, res) => {};
+export const getUser = (req, res) => {
+    pool.query('select * from users', (error, results)=> {
+        if (error) {
+            res.status(500).json({msg: error, users: []});
+            return;
+        }
+        res.status(200).json({msg: "OK",  users: results });
+});
+
+};
 export const postUser = (req, res) => {} ;
 export const putUser = (req, res) => {} ;
 export const deleteUser = (req, res) => {} ;
