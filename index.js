bbb11a3d977df7ceb98616776b1f59e85af0662a
robertopeiro12 <robertopeiro12@gmail.com>
2025-05-15 09:10:04 -0600

    feat: add morgan and cors middleware for logging and CORS support

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import "dotenv/config";
 import express from "express";
 import indexRoutes from "./routes/index.routes.js";
 import UserRoutes from "./routes/users.routes.js"
+import morgan from "morgan";
+import cors from "cors";
 
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 app.use(indexRoutes);
 app.use(UserRoutes);
 const port = 5001;
